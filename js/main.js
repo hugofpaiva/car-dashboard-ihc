28b41bad58050dc9b90cbea16cfe124769a8c728
hugofpaiva <hugofpaiva@ua.pt>
2020-05-15 20:31:00 +0100

    Organização do ecrã inicial feito juntamento com funções de hora e tempo

--- js/main.js
+++ js/main.js
@@ -1,42 +1,4 @@
-var date = new Date();
-var hour = date.getHours();
-var minutes = date.getMinutes();
-if (minutes.length < 2) {
-  minutes = "0" + minutes;
-}
-
-var days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-var months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-var day = date.getDay();
-var dayWeek = days[date.getDay()];
-var month = months[date.getMonth()];
-var year = date.getFullYear();
-
-document.getElementById("hours").innerHTML = hour + ":" + minutes;
-document.getElementById("date").innerHTML =
-  dayWeek + ", " + day + " " + month + " " + year;
-
+// FUNÇÕES DE TEMPO
 // get location
 function getLocation() {
   if (navigator.geolocation) {
@@ -54,9 +16,65 @@ function getWeather(lat, long) {
   fetch(`${root}lat=${lat}&lon=${long}`, { method: "get" })
     .then((resp) => resp.json())
     .then((data) => {
-        console.log(data.name, data.weather, data.main.temp);    
+      console.log(data.name, data.weather, data.main.temp);
+      document.getElementById("temperature").innerHTML = data.main.temp.toFixed(
+        0
+      );
     })
     .catch(function (err) {
       console.error(err);
     });
 }
+
+getLocation();
+
+// HORAS
+
+function updateTime() {
+  var days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  var months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  var date = new Date();
+  var hour = date.getHours();
+  var minutes = date.getMinutes();
+  var day = date.getDay();
+  var dayWeek = days[day];
+  var month = months[date.getMonth()];
+  var year = date.getFullYear();
+  if (minutes.length < 2) {
+    minutes = "0" + minutes;
+  }
+
+  document.getElementById("hours").innerHTML = hour + ":" + minutes;
+  document.getElementById("date").innerHTML =
+    dayWeek + ", " + day + " " + month + " " + year;
+}
+
+// FUNÇÕES DE MANUTENÇÃO
+
+function main(){
+    getLocation();
+    updateTime();
+}
+
+setInterval(main, 10000);
