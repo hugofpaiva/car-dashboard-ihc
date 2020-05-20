71183c22a0f9e1368e80e09b3ff7abcd93e7d0d2
hugofpaiva <hugofpaiva@ua.pt>
2020-05-20 17:10:02 +0100

    Fixes

--- js/main.js
+++ js/main.js
@@ -21,17 +21,7 @@ function init() {
     }
 
     if (document.querySelector("#phone")) {
-      var recentcalls = [
-        ["./img/pedro.jpeg", "Pedro<br>today"],
-        ["./img/fabiana.jpeg", "Fabiana<br>yesterday"],
-        ["./img/alberto.jpeg", "Alberto<br>yesterday"],
-        ["./img/alfredo.jpeg", "Alfredo<br>yesterday"],
-      ];
-
-      for (i = 0; i < 4; i++) {
-        document.getElementById("desc" + i).innerHTML = recentcalls[i][1];
-        document.getElementById("im" + i).src = recentcalls[i][0];
-      }
+      phone()
     }
 
     if (document.querySelector("#map")) {
